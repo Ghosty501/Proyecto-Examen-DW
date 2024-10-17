0ad92b319ed84ef59b99d3417c15777866e161c6
Armando <armanguzman27@gmail.com>
2024-10-16 22:30:51 -0600

    Correciones

--- components/nav-tabs.php
+++ components/nav-tabs.php
@@ -1,14 +1,18 @@
 <div class="mt-3 tabs">
     <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="resultados-tab" data-bs-toggle="tab" data-bs-target="Resultados.php" type="button" role="tab">Resultados</button>
+            <button class="nav-link active" id="resultados-tab" data-bs-toggle="tab" data-bs-target="#resultados-tab-pane" type="button" role="tab">Resultados</button>
         </li>
         <li class="nav-item" role="presentation">
-            <button class="nav-link" id="categoria-tab" data-bs-toggle="tab" data-bs-target="Categoria.php" type="button" role="tab">Categorías</button>
+            <button class="nav-link" id="categoria-tab" data-bs-toggle="tab" data-bs-target="#categoria-tab-pane" type="button" role="tab">Categorías</button>
         </li>
     </ul>
     <div class="tab-content" id="myTabContent">
-        <div class="tab-pane fade show active" id="resultados-tab-pane" role="tabpanel" tabindex="0"><?php include 'Resultados.php'?></div>
-        <div class="tab-pane fade" id="categoria-tab-pane" role="tabpanel" tabindex="0"><?php include 'Categoria.php'?></div>
+        <div class="tab-pane fade show active" id="resultados-tab-pane" role="tabpanel" tabindex="0">
+            <?php include 'Resultados.php'; ?>
+        </div>
+        <div class="tab-pane fade" id="categoria-tab-pane" role="tabpanel" tabindex="0">
+            <?php include 'Categoria.php'; ?>
+        </div>
     </div>
 </div>
--- components/resumen.php
+++ components/resumen.php
@@ -1,4 +1,21 @@
-<div class="cinta-resumen mt-3">
+<div class="cinta-resumen mt-3 text-center">
+    <div class="row">
+        <div class="col" id="sesiones">
+            0
+        </div>
+        <div class="col" id="hrs-profesor">
+            0
+        </div>
+        <div class="col" id="duracion-media">
+            0
+        </div>
+        <div class="col" id="hrs-talent">
+            0
+        </div>
+        <div class="col" id="profesores">
+            0
+        </div>
+    </div>
     <div class="row">
         <div class="col">
             Sesiones
@@ -15,4 +32,5 @@
         <div class="col">
             Profesores
         </div>
+    </div>
 </div>
