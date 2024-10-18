0cb72287146920499f187247217e33b34fec8444
Ghosty501 <0253260@up.edu.mx>
2024-10-18 02:09:18 -0600

    Ya ando cansado

--- components/resumen.php
+++ components/resumen.php
@@ -1,36 +1,20 @@
 <div class="cinta-resumen mt-3 text-center">
     <div class="row">
-        <div class="col" id="sesiones">
-            0
-        </div>
-        <div class="col" id="hrs-profesor">
-            0
-        </div>
-        <div class="col" id="duracion-media">
-            0
-        </div>
-        <div class="col" id="hrs-talent">
-            0
-        </div>
-        <div class="col" id="profesores">
-            0
-        </div>
+        <div class="col" id="sesiones">0</div>
+        <div class="col" id="hrs-profesor">0</div>
+        <div class="col" id="duracion-media">0</div>
+        <div class="col" id="hrs-talent">0</div>
+        <div class="col" id="profesores">0</div>
     </div>
     <div class="row">
-        <div class="col">
-            Sesiones
-        </div>
-        <div class="col">
-            Total Hrs. Profesor
-        </div>
-        <div class="col">
-            Duración Media Sesión
-        </div>
-        <div class="col">
-            Total Hrs. Talent
-        </div>
-        <div class="col">
-            Profesores
-        </div>
+        <div class="col">Sesiones</div>
+        <div class="col">Total Hrs. Profesor</div>
+        <div class="col">Duración Media Sesión</div>
+        <div class="col">Total Hrs. Talent</div>
+        <div class="col">Profesores</div>
     </div>
 </div>
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src = "js/resumen.js"></script>
